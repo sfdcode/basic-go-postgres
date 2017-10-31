21f0b87d3d7302be89aaa91e48d0ea3fc7341580
sfdcode <sfdcode@gmail.com>
2017-10-31 12:15:14 +0100

    insert into db

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -12,25 +13,52 @@ import (
 	_ "github.com/lib/pq"
 )
 
+type Notification struct {
+	ID          string `xml:"Id"`
+	SObjectID   string `xml:"sObject>Id"`
+	SObjectName string `xml:"sObject>Name"`
+}
+type Result struct {
+	Notifications []Notification `xml:"Body>notifications>Notification"`
+}
+
 var (
 	db  *sql.DB
 	err error
+	ack = `<?xml version="1.0" encoding="UTF-8"?>
+			<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+			<soap:Body>
+				<notificationsResponse xmlns:ns2="urn:sobject.enterprise.soap.sforce.com" xmlns="http://soap.sforce.com/2005/09/outbound">
+				<Ack>true</Ack>
+				</notificationsResponse>
+			</soap:Body>
+		</soap:Envelope>`
+
+	nack = `<?xml version="1.0" encoding="UTF-8"?>
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      <soapenv:Body>
+        <soapenv:Fault>
+          <faultcode>soap:Receiver</faultcode>
+          <faultstring>%s</faultstring>
+        </soapenv:Fault>
+      </soapenv:Body>
+    </soapenv:Envelope>`
 )
 
 func init() {
 	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	//db, err = sql.Open("postgres", "user=jsuarez password=welcome1 dbname=jsuarez sslmode=disable")
 	if err != nil {
 		log.Fatalf("Error opening database: %q", err)
 	}
 
-	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS messages (id varchar)"); err != nil {
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS messages (sfid varchar, account_name varchar)"); err != nil {
 		log.Fatalf("Error creating table: %q", err)
 	} else {
 		fmt.Println("Table initialited")
 	}
 }
 
-// Logger
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
@@ -49,14 +77,38 @@ func Logger(inner http.Handler, name string) http.Handler {
 
 var myHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
+	resp := ack
 	if err != nil {
 		log.Fatalf("Error reading body: %q", err)
+		resp = fmt.Sprintf(nack, "Error reading body")
 	}
 
 	fmt.Printf("body %q\n", string(body))
 
-	w.Header().Add("content-type", "text/html")
-	w.Write([]byte("<h2>hello world</h2>"))
+	v := &Result{}
+
+	err = xml.Unmarshal(body, v)
+
+	for _, val := range v.Notifications {
+
+		stmt, err := db.Prepare(fmt.Sprintf("insert into messages values('%v','%v')", val.SObjectID, val.SObjectName))
+		if err != nil {
+			log.Fatalf("Error inserting message: %q", err)
+			resp = fmt.Sprintf(nack, "Error inserting message")
+		}
+
+		defer stmt.Close()
+
+		_, err = stmt.Exec()
+		if err != nil {
+			log.Fatalf("Error inserting message: %q", err)
+			resp = fmt.Sprintf(nack, "Error inserting message")
+			break
+		}
+	}
+
+	w.Header().Add("content-type", "text/xml")
+	w.Write([]byte(resp))
 })
 
 func main() {
@@ -74,4 +126,5 @@ func main() {
 	fmt.Printf("Server started and listening in the port: %v\n", port)
 
 	log.Fatal(s.ListenAndServe())
+
 }
